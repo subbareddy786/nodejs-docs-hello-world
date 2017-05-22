60a9e780517dea2b84bdb6977b9b51b394ca1c1e
VENKATA SUBBA REDDY YERRAGUDI <subbareddy786@gmail.com>
2017-05-21 22:43:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello Azure!");
-    response.end("Hello World!");`.
+    response.end("Hello World!");
 
 });
 
